2008843c8f0fdb6deae62217fe15d56cfd78d657
Ngwa Precious <ngwap345@gmail.com>
2024-07-12 11:55:11 +0100

    Add function for random Computer choice

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ document.getElementById("roll").onclick = function () {
   //   console.log(y);
   document.getElementById("choice").innerHTML = y;
 };
+
+function getComputerChoice() {
+  y = Math.floor(Math.random() * 3) + 1;
+  if (y === 1) {
+    y = "Rock";
+  } else if (y === 2) {
+    y = "Paper";
+  } else if (y === 3) {
+    y = "Scissors";
+  }
+  console.log(y);
+}
