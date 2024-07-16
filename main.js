680a4a76ffaf0076a49dc7b6248226d352ac768a
Ngwa Precious <ngwap345@gmail.com>
2024-07-16 13:14:09 +0100

    Change alert for player won or lost

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const humanAnswer = getHumanChoice();
+let computerAnswer = getComputerChoice();
 //Players Score
 let humanScore = 0;
 let computerScore = 0;
@@ -5,8 +7,7 @@ let computerScore = 0;
 document.getElementById("roll").onclick = function () {
   // y = Math.floor(Math.random() * 3) + 1;
   //   console.log(y);
-
-  document.getElementById("choice").innerHTML = getHumanChoice();
+  // document.getElementById("choice").innerHTML = getHumanChoice();
 };
 
 //Funtion For Computer to make a random choice of rock, paper, scissors.
@@ -20,43 +21,50 @@ function getComputerChoice() {
   } else if (y === 3) {
     y = "scissors";
   }
-  console.log(y);
+  console.log(`Computer value ${y}`);
   return y;
 }
 
 //Funtion to take Human choice.
 function getHumanChoice() {
   let userChoice = prompt("Please Enter your choice - Rock, Paper, Scissors!");
-  console.log(userChoice);
-  return userChoice;
+  console.log(`My choice ${userChoice}`);
+  if (userChoice === "rock") {
+    return userChoice;
+  } else if (userChoice === "paper") {
+    return userChoice;
+  } else if (userChoice === "scissors") {
+    return userChoice;
+  } else {
+    alert.window("Invalid");
+  }
 }
-
-const humanAnswer = getHumanChoice();
-const computerAnswer = getComputerChoice();
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     window.alert(`It's a draw`);
   } else if (humanChoice === "rock" && computerChoice === "paper") {
     computerScore += 1;
+    window.alert("Computer won the round");
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
     humanScore += 1;
+    window.alert("Human won the round");
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
     computerScore += 1;
+    window.alert("Computer won the round");
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
     humanScore += 1;
+    window.alert("Human won the round");
   } else if (humanChoice === "paper" && computerChoice === "rock") {
     humanScore += 1;
+    window.alert("Human won the round");
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
     computerScore += 1;
+    window.alert("Computer won the round");
   }
 
   // If statements to determine winner of the round
-  if (computerScore > humanScore) {
-    window.alert("Computer won the round");
-  } else if (computerScore < humanScore) {
-    window.alert("Human won the round");
-  } else if (computerScore == 5) {
+  else if (computerScore == 5) {
     window.alert("Sorry You lost the game. :(");
   } else if (humanScore == 5) {
     window.alert("Hooray! You won the Game. :)");
@@ -66,6 +74,7 @@ function playRound(humanChoice, computerChoice) {
 function playGame() {
   x = 0;
   while (x < 5) {
+    getHumanChoice();
     playRound(humanAnswer, computerAnswer);
     x += 1;
   }
